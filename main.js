1d1cba66bee1baa01a133a9a8ead0d0574b2639b
Dylan R <d.raiff@icloud.com>
2021-09-23 10:34:11 -0400

    added number input

--- main.js
+++ main.js
@@ -9,6 +9,16 @@ function set_img_src(index)
 
 function on_press()
 {
+    var num_rolls = document.getElementById("num_dice").value;
+
+    for (var i = 0; i < num_rolls; i++)
+    {
+        setTimeout(roll, 1000 * i);
+    }
+}
+
+function roll()
+{
     var index = Math.round(Math.random() * 5) + 1;
     set_img_src(index);
 }
