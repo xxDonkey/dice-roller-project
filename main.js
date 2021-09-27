dc832f9172d2fca27f45fe0a1cb0f242c43ed477
Dylan R <d.raiff@icloud.com>
2021-09-27 11:02:55 -0400

    f

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 const img_folder = "img"
 const file_name = "dice"
+
+var sides = "six";
+var reg_mode = true;
     
 function set_img_src(index)
 {
-    var src = img_folder + "/" + file_name + index + ".png";
-    document.getElementById("dice").src = src;
+    var src = img_folder + "/" + sides + "/" + file_name + index + ".png";
+    document.getElementById("die").src = src;
 }
 
-function on_press()
+function roll()
+{
+    
+    var index = Math.round(Math.random() * 5) + 1;
+    set_img_src(index);
+}
+
+function on_roll_die()
 {
     var num_rolls = document.getElementById("num_dice").value;
 
@@ -17,8 +27,13 @@ function on_press()
     }
 }
 
-function roll()
+function switch_mode()
 {
-    var index = Math.round(Math.random() * 5) + 1;
-    set_img_src(index);
+    reg_div = document.getElementById("reg_mode");
+    reg_div.hidden = !reg_div.hidden;
+
+    vs_div = document.getElementById("vs_mode");
+    vs_div.hidden = !vs_div.hidden;
+
+    reg_mode = !reg_mode;
 }
