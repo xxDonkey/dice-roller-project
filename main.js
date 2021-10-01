113c55ce9c09743e1852c6417b9dc55ade6a78ab
Dylan R <d.raiff@icloud.com>
2021-10-01 14:12:27 -0400

    added documentation comments

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const img_folder = "img"
 const file_name = "dice"
-    
+
+// Sets the src of the <img> tag with the given ID to the corresponding face.
 function set_img_src(index, id)
 {
     var src = img_folder + "/" + file_name + index + ".png"
     document.getElementById(id).src = src
 }
 
+// Creates a div housing 2 die <img> tags
 function create_img_div(i)
 {
+    // create the div
     var div = document.createElement("div")
     div.setAttribute("class", "dice")
 
+    // create the first img
     var img1 = document.createElement("img")
     img1.setAttribute("id", i + "die1")
     img1.setAttribute("width", 256)
@@ -19,6 +23,7 @@ function create_img_div(i)
     img1.setAttribute("src", "img/dice1.png")
     div.appendChild(img1)
 
+    // create the second img
     var img2 = document.createElement("img")
     img2.setAttribute("id", i + "die2")
     img2.setAttribute("width", 256)
@@ -26,28 +31,33 @@ function create_img_div(i)
     img2.setAttribute("src", "img/dice1.png")
     div.appendChild(img2)
 
+    // append to the webpage
     document.body.appendChild(div)
 }
 
 function roll()
 {
+    // read info from webpage
     var num_dice = parseInt(document.getElementById("num_dice").value)
     var faces = parseInt(document.getElementById("num_sides").value)
     var img_divs = document.getElementsByClassName("dice")  
     
+    // if we dont have enough dice, create more
     if (num_dice > img_divs.length)
         for (var i = img_divs.length; i < num_dice; i++)
         {
             create_img_div(i)
         }
 
+    // if we have too many, delete extra
     else if (num_dice < img_divs.length)
         for (var i = img_divs.length - 1; i >= num_dice; i--)
         {
-            console.log(i)
             img_divs[i].remove()
         }
     
+    // clamp value of 'faces' to between 1 and 6
+
     if (faces < 1)
         document.getElementById("num_sides").value = 1
 
@@ -57,10 +67,9 @@ function roll()
     var t1 = 0
     var t2 = 0
 
+    // for each div, roll the 2 dice, add their values to t1 and t2
     for (var i = 0; i < num_dice; i++)
     {
-        console.log(i)
-
         var index1 = Math.round(Math.random() * (faces - 1)) + 1
         set_img_src(index1, i + "die1")
         t1 += index1
@@ -70,9 +79,11 @@ function roll()
         t2 += index2
     }
 
+    // set the score text
     document.getElementById("score1").innerHTML = t1
     document.getElementById("score2").innerHTML = t2
 
+    // set the winner text
     if (t1 < t2)
         document.getElementById("winner").innerHTML = "Player 2 wins!"
 
@@ -81,6 +92,5 @@ function roll()
     
     else
         document.getElementById("winner").innerHTML = "It's a tie!"
-    
 }
 
