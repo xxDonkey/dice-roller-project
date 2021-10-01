3972069569b3623b63ea664a5723a583f7b9236d
Dylan R <d.raiff@icloud.com>
2021-10-01 10:48:02 -0400

    c

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function create_img_div(i)
     div.appendChild(img1)
 
     var img2 = document.createElement("img")
-    img2.setAttribute("id", i + "die1")
+    img2.setAttribute("id", i + "die2")
     img2.setAttribute("width", 256)
     img2.setAttribute("height", 256)
     img2.setAttribute("src", "img/dice1.png")
@@ -33,10 +33,7 @@ function roll()
 {
     var num_dice = parseInt(document.getElementById("num_dice").value)
     var img_divs = document.getElementsByClassName("dice")  
-
-    console.log(num_dice, img_divs.length)
     
-    // 4 > 2
     if (num_dice > img_divs.length)
         for (var i = img_divs.length; i < num_dice; i++)
         {
@@ -44,31 +41,36 @@ function roll()
         }
 
     else if (num_dice < img_divs.length)
-        for (var i = num_dice; i < img_divs.length; i++)
+        for (var i = img_divs.length - 1; i >= num_dice; i--)
         {
-            //img_divs[i].remove()
+            console.log(i)
+            img_divs[i].remove()
         }
 
+    var t1 = 0
+    var t2 = 0
 
-    var index1 = Math.round(Math.random() * 5) + 1
-    set_img_src(index1, "1die1")
-    
-    var index2 = Math.round(Math.random() * 5) + 1
-    set_img_src(index2, "1die2")
-
-
-
-    if (index1 < index2)
+    for (var i = 0; i < num_dice; i++)
     {
+        console.log(i)
+
+        var index1 = Math.round(Math.random() * 5) + 1
+        set_img_src(index1, i + "die1")
+        t1 += index1
+        
+        var index2 = Math.round(Math.random() * 5) + 1
+        set_img_src(index2, i + "die2")
+        t2 += index2
+    }
+
+    if (t1 < t2)
         document.getElementById("winner").innerHTML = "Player 2 wins!"
-    }
-    else if (index1 > index2)
-    {
+
+    else if (t1 > t2)
         document.getElementById("winner").innerHTML = "Player 1 wins!"
-    }
+    
     else
-    {
         document.getElementById("winner").innerHTML = "It's a tie!"
-    }
+    
 }
 
